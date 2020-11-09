4eb17e3bcfd0622adf420e3c7c9cb9c2c2ed8c36
TVD <thevalenciandev@gmail.com>
2020-11-09 19:00:33 +0000

    Exercise 2.4: Higher order functions (compose function of triple and double)

---
+++ src/main/java/com/embosfer/fpinjava/chapter2/Exercise2_4.java
@@ -0,0 +1,17 @@
+package com.embosfer.fpinjava.chapter2;
+
+public class Exercise2_4 {
+
+    public static void main(String[] args) {
+        Function<Integer, Integer> triple = x -> x * 3;
+        Function<Integer, Integer> square = x -> x * x;
+
+        Function<Function<Integer, Integer>,
+                Function<Function<Integer, Integer>,
+                        Function<Integer, Integer>>> compose = f1 -> f2 -> x -> f1.apply(f2.apply(x));
+
+        Function<Integer, Integer> tripleItThenSquareIt = compose.apply(square).apply(triple);
+
+        System.out.println(tripleItThenSquareIt.apply(2));
+    }
+}
