62fb4a853bf25b886d123c6de7d88cd8e3fdf4cc
TVD <thevalenciandev@gmail.com>
2020-11-15 17:07:31 +0000

    Refactor of exercise 2.7

--- src/main/java/com/embosfer/fpinjava/chapter2/Functions.java
+++ src/main/java/com/embosfer/fpinjava/chapter2/Functions.java
@@ -2,6 +2,10 @@ package com.embosfer.fpinjava.chapter2;
 
 public final class Functions {
 
+    public static <A, B, C> Function<B, C> partialA(A a, Function<A, Function<B, C>> f) {
+        return f.apply(a);
+    }
+
     public static <A, B, C> Function<A, C> partialB(B b, Function<A, Function<B, C>> f) {
         return a -> f.apply(a).apply(b);
     }
--- src/test/java/com/embosfer/fpinjava/chapter2/FunctionsTest.java
+++ src/test/java/com/embosfer/fpinjava/chapter2/FunctionsTest.java
@@ -2,9 +2,17 @@ package com.embosfer.fpinjava.chapter2;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FunctionsTest {
+
+    @Test
+    void exercise_2_7_partiallyApplyingFirstArgToACurriedFunctionOfTwoArgs() {
+        Function<Integer, Function<Integer, Integer>> f = x -> y -> x + y;
+
+        Function<Integer, Integer> g = Functions.partialA(5, f);
+        assertEquals(10, g.apply(5));
+    }
 
     @Test
     void exercise_2_8_partiallyApplyingSecondArgToACurriedFunctionOfTwoArgs() {
