c97880005934f263f5cc73c03d32bdb7cd01debd
TVD <thevalenciandev@gmail.com>
2020-11-15 18:04:44 +0000

    Exercise 2.10: Currying a function of two params represented as a tuple

--- src/main/java/com/thevalenciandev/fpinjava/chapter2/Functions.java
+++ src/main/java/com/thevalenciandev/fpinjava/chapter2/Functions.java
@@ -1,5 +1,7 @@
 package com.thevalenciandev.fpinjava.chapter2;
 
+import com.thevalenciandev.fpinjava.chapter1.Tuple;
+
 public final class Functions {
 
     public static <A, B, C> Function<B, C> partialA(A a, Function<A, Function<B, C>> f) {
@@ -8,5 +10,9 @@ public final class Functions {
 
     public static <A, B, C> Function<A, C> partialB(B b, Function<A, Function<B, C>> f) {
         return a -> f.apply(a).apply(b);
+    }
+
+    public static <A, B, C> Function<A, Function<B, C>> curry(Function<Tuple<A, B>, C> f) {
+        return a -> b -> f.apply(new Tuple<>(a, b));
     }
 }
--- src/test/java/com/thevalenciandev/fpinjava/chapter2/FunctionsTest.java
+++ src/test/java/com/thevalenciandev/fpinjava/chapter2/FunctionsTest.java
@@ -1,5 +1,6 @@
 package com.thevalenciandev.fpinjava.chapter2;
 
+import com.thevalenciandev.fpinjava.chapter1.Tuple;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,6 +21,13 @@ class FunctionsTest {
 
         Function<Integer, Integer> divideByTwo = Functions.partialB(2, f);
         assertEquals(5, divideByTwo.apply(10));
+    }
+
+    @Test
+    void exercise_2_10_curryingAFunctionOfTwoArgsModeledAsATuple() {
+        Function<Tuple<Integer, Integer>, Integer> f = t -> t._1 + t._2;
+
+        assertEquals(f.apply(new Tuple<>(6, 6)), Functions.curry(f).apply(6).apply(6));
     }
 
 }
