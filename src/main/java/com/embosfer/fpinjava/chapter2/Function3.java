96f32f3dea88def043111340f3148c23c2ebf0ce
TVD <thevalenciandev@gmail.com>
2020-11-15 16:36:10 +0000

    A function of three parameters (addTax) done with currying

---
+++ src/main/java/com/embosfer/fpinjava/chapter2/Function3.java
@@ -0,0 +1,16 @@
+package com.embosfer.fpinjava.chapter2;
+
+/**
+ * Done with currying...
+ */
+public class Function3 {
+
+    public static void main(String[] args) {
+
+        // Given a tax rate (double) and a price (double), return price with added tax (double)
+        Function<Double, Function<Double, Double>> addTax = taxRate -> price -> price + price * taxRate;
+
+        System.out.println(addTax.apply(0.09).apply(12.00));
+    }
+
+}
