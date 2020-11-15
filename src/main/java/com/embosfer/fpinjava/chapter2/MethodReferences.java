22f85416186fdabc1ff86e683657803590222744
TVD <thevalenciandev@gmail.com>
2020-11-15 14:19:56 +0000

    Anonymous functions and method references

---
+++ src/main/java/com/embosfer/fpinjava/chapter2/MethodReferences.java
@@ -0,0 +1,15 @@
+package com.embosfer.fpinjava.chapter2;
+
+public class MethodReferences {
+
+    static <T, U, V> Function<T, V> compose(Function<T, U> f, Function<U, V> g) {
+        return x -> g.apply(f.apply(x));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MethodReferences.compose(
+                (Double z) -> Math.PI / 2 - z,
+                Math::sin)
+                .apply(3.0));
+    }
+}
---
+++ src/test/java/com/embosfer/fpinjava/chapter2/Exercise2_5Test.java
@@ -0,0 +1,15 @@
+package com.embosfer.fpinjava.chapter2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Exercise2_5Test {
+
+    @Test
+    void canUseAnonymousFunctions() {
+        Double cosineOfTwo = Exercise2_5.<Double, Double, Double>higherCompose().apply(x -> Math.PI / 2 - x).apply(Math::sin).apply(2.0);
+        assertEquals(0.6614988999692148, cosineOfTwo);
+    }
+
+}
