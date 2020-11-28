a7c42d6144d48a4f7ff2b7316e6dcb6eb85ed402
TVD <thevalenciandev@gmail.com>
2020-11-28 18:26:56 +0000

    Exercise 3.3: APIs for creation of immutable lists

--- src/main/java/com/thevalenciandev/fpinjava/chapter3/CollectionUtilities.java
+++ src/main/java/com/thevalenciandev/fpinjava/chapter3/CollectionUtilities.java
@@ -1,12 +1,33 @@
 package com.thevalenciandev.fpinjava.chapter3;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 
+/**
+ * This is less useful on newer versions of Java containing the List.of API
+ */
 public class CollectionUtilities {
 
-    static <T, U> List<U> map(List<T> list, Function<T, U> f) {
+    public static <E> List<E> list() {
+        return Collections.emptyList();
+    }
+
+    public static <E> List<E> list(E elem) {
+        return Collections.singletonList(elem);
+    }
+
+    public static <E> List<E> list(List<E> other) {
+        return Collections.unmodifiableList(new ArrayList<>(other));
+    }
+
+    public static <E> List<E> list(E... elements) {
+        return Collections.unmodifiableList(Arrays.asList(Arrays.copyOf(elements, elements.length)));
+    }
+
+    public static <T, U> List<U> map(List<T> list, Function<T, U> f) {
         List<U> newList = new ArrayList<>();
         list.forEach(val -> newList.add(f.apply(val)));
         return newList;
--- src/test/java/com/thevalenciandev/fpinjava/chapter3/CollectionUtilitiesTest.java
+++ src/test/java/com/thevalenciandev/fpinjava/chapter3/CollectionUtilitiesTest.java
@@ -2,6 +2,9 @@ package com.thevalenciandev.fpinjava.chapter3;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 
@@ -11,10 +14,45 @@ import static org.junit.jupiter.api.Assertions.*;
 class CollectionUtilitiesTest {
 
     @Test
+    void testEmptyList() {
+        List<Double> list = CollectionUtilities.list();
+        assertEquals(Collections.emptyList(), list);
+        assertThrows(UnsupportedOperationException.class, () -> list.add(1.0));
+    }
+
+    @Test
+    void testListWithOneElement() {
+        List<String> list = CollectionUtilities.list("one");
+        assertEquals(List.of("one"), list);
+        assertThrows(UnsupportedOperationException.class, () -> list.add("two"));
+    }
+
+   @Test
+   void testListFromCollectionOfElements() {
+       List<Integer> other = makeMutableListOf(1, 2, 3);
+       List<Integer> list = CollectionUtilities.list(other);
+       assertEquals(other, list);
+       assertNotSame(other, list);
+
+       other.add(4);
+       assertNotEquals(other, list);
+   }
+
+   @Test
+   void testListOfVarArgs() {
+       List<String> list = CollectionUtilities.list("one", "two");
+       assertEquals(List.of("one", "two"), list);
+       assertThrows(UnsupportedOperationException.class, () -> list.add("two"));
+   }
+
+    @Test
     void testMap() {
         List<Integer> integers = List.of(1, 2);
         Function<Integer, Double> addTwentyPercent = x -> x * 1.2d;
         assertEquals(List.of(1.2, 2.4), map(integers, addTwentyPercent));
     }
 
+    private <T> List<T> makeMutableListOf(T... elem) {
+        return new ArrayList<>(Arrays.asList(elem));
+    }
 }
