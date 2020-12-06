15da976fa11c5a50a21f082b67e2f3212276303c
TVD <thevalenciandev@gmail.com>
2020-12-06 10:30:28 +0000

    Playing with types II: using value types to leverage on compiler for more safety on writing programs

--- src/test/java/com/thevalenciandev/fpinjava/chapter3/Store.java
+++ src/test/java/com/thevalenciandev/fpinjava/chapter3/Store.java
@@ -7,29 +7,56 @@ import static com.thevalenciandev.fpinjava.chapter3.CollectionUtilities.list;
 
 public class Store {
 
-    record Product(String name, double price, double weight) {}
+    record Price(double value) {
+
+        public static final Price ZERO = new Price(0.0);
+
+        public Price add(Price that) {
+            return new Price(this.value + that.value);
+        }
+
+        public Price mult(int count) {
+            return new Price(this.value * count);
+        }
+
+    }
+
+    record Weight(double value) {
+
+        public static final Weight ZERO = new Weight(0.0);
+
+        public Weight add(Weight that) {
+            return new Weight(this.value + that.value);
+        }
+
+        public Weight mult(int count) {
+            return new Weight(this.value * count);
+        }
+    }
+
+    record Product(String name, Price price, Weight weight) {}
 
     record OrderLine(Product product, int count) {
 
-        public double weight() {
-            return product.weight() * count;
+        public Weight weight() {
+            return product.weight().mult(count);
         }
 
-        public double amount() {
-            return product.price() * count;
+        public Price amount() {
+            return product.price().mult(count);
         }
 
     }
 
     public static void main(String[] args) {
-        Product toothPaste = new Product("Tooth paste", 1.5, 0.5);
-        Product toothBrush = new Product("Tooth brush", 3.5, 0.3);
+        Product toothPaste = new Product("Tooth paste", new Price(1.5), new Weight(0.5));
+        Product toothBrush = new Product("Tooth brush", new Price(3.5), new Weight(0.3));
         List<OrderLine> order = list(
                 new OrderLine(toothPaste, 2),
                 new OrderLine(toothBrush, 3));
 
-        double weight = foldLeft(order, 0.0, x -> y -> x + y.weight());
-        double price = foldLeft(order, 0.0, x -> y -> x + y.amount());
+        Weight weight = foldLeft(order, Weight.ZERO, x -> y -> x.add(y.weight()));
+        Price price = foldLeft(order, Price.ZERO, x -> y -> x.add(y.amount()));
 
         System.out.println(String.format("Total weight: %s", weight));
         System.out.println(String.format("Total price: %s", price));
